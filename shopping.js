08b6ea84b1123ac68d09f7c2e6add2b532584ff0
rasel <russelroy15@gmail.com>
2021-09-05 14:28:06 +0600

    add new code

--- shopping.js
+++ shopping.js
@@ -4,7 +4,7 @@ const addItem = () => {
   //display in the ui
   displayProduct(name);
   //add to local storage
-
+  addProductToCart(name);
   nameField.value = "";
 };
 
@@ -13,4 +13,23 @@ const displayProduct = (name) => {
   const li = document.createElement("li");
   li.innerText = name;
   ul.appendChild(li);
+};
+
+const getCart = () => {
+  const cart = localStorage.getItem("cart");
+  let cartObj;
+  if (cart) {
+    cartObj = JSON.parse(cart);
+  } else {
+    cartObj = {};
+  }
+  return cartObj;
+};
+
+const addProductToCart = (name) => {
+  const cart = getCart();
+  cart[name] = 1;
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringified);
+  console.log(cart);
 };
