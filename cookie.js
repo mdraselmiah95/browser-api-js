78660cefe0ce779d0f091a05b7436dd620baf73f
rasel <russelroy15@gmail.com>
2021-09-05 13:35:37 +0600

    learning cookie more

--- cookie.js
+++ cookie.js
@@ -8,3 +8,13 @@ const getCookie = (name) => {
     return cookieNameValue[1];
   }
 };
+
+const cookies = (name) => {
+  const cookie = document.cookie;
+  const allCookie = cookie.split(";");
+  const findCookie = allCookie.find((c) => c.includes(name));
+  if (findCookie) {
+    const cookieNameValue = findCookie.split("=");
+    return cookieNameValue[1];
+  }
+};
