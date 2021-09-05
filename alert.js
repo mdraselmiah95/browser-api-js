51b29c3c3151ca1d5e41c3775b9f799c1a1e5ea2
rasel <russelroy15@gmail.com>
2021-09-05 12:24:33 +0600

    added new code

--- alert.js
+++ alert.js
@@ -1,2 +1,15 @@
 console.log("Please understand");
 // alert("Something is coming");
+const maComing = () => {
+  alert("Please someone coming");
+};
+
+const askPicnic = () => {
+  const response = confirm("Are you going to picnic?");
+  console.log(response);
+  if (response === true) {
+    alert("Send me money via Bkash");
+  } else {
+    console.log("You have to wait.");
+  }
+};
